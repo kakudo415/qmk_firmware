0658713b9f2d69e761072b6416bb79a9b88570ba
Kakudo <kakudo415@gmail.com>
2020-10-21 00:32:14 +0900

    通信できた

--- keyboards/4421/config.h
+++ keyboards/4421/config.h
@@ -27,8 +27,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define PRODUCT      4421
 
 /* key matrix size */
-#define MATRIX_ROWS 2
-#define MATRIX_COLS 3
+#define MATRIX_ROWS 8
+#define MATRIX_COLS 6
 
 /*
  * Keyboard Matrix Assignments
@@ -40,8 +40,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *                  ROW2COL = ROW = Anode (+), COL = Cathode (-, marked on diode)
  *
  */
-#define MATRIX_ROW_PINS { D0, D5 }
-#define MATRIX_COL_PINS { F1, F0, B0 }
+#define MATRIX_ROW_PINS { F0, B4, D7, D6 }
+#define MATRIX_COL_PINS { C6, B6, B5, F7, F4, F5 }
 #define UNUSED_PINS
 
 /* COL2ROW, ROW2COL */
@@ -50,7 +50,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /*
  * Split Keyboard specific options, make sure you have 'SPLIT_KEYBOARD = yes' in your rules.mk, and define SOFT_SERIAL_PIN.
  */
-#define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
+// #define SOFT_SERIAL_PIN D0  // or D1, D2, D3, E6
 
 //#define BACKLIGHT_PIN B7
 //#define BACKLIGHT_LEVELS 3
---
+++ keyboards/4421/keymaps/default/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define USE_I2C
+
+#define MASTER_LEFT
+#define SPLIT_USB_DETECT
