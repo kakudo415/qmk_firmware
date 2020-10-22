0aa339f84166770bfe56d26bc98ce2dbc80cf28b
Kakudo <kakudo415@gmail.com>
2020-10-22 21:27:41 +0900

    ESCをRaiseレイヤーに追加

--- keyboards/4421/keymaps/default/keymap.c
+++ keyboards/4421/keymaps/default/keymap.c
@@ -43,10 +43,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,     RESET,   KC_NO,      KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,      RESET,      KC_NO
     ),
     [_RAISE] = LAYOUT(
-        KC_F1,     KC_F2,     KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_F11,  KC_F12,  \
-        KC_NO,     KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_NO,   KC_NO,   \
-        KC_LSFT,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_RSFT, \
-        KC_NO,     KC_LALT,   RESET,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   RESET,     KC_RALT, KC_NO
+        KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,   KC_F8,   KC_F9, KC_F10,   KC_F11, KC_F12,  \
+        KC_ESC,  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_NO,  KC_NO,   \
+        KC_LSFT, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,    KC_NO,  KC_RSFT, \
+        KC_NO,   RESET, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,    RESET,  KC_NO
     )
 };
 
